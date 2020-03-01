4be97ad695b40f307d720fd47af5600457d67667
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-03-01 16:24:59 +0000

    iterate over card components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: "Neat Tree",
+      imageUrl: "/assets/tree.jpeg",
+      username: "nature",
+      content: "I saw a tree"
+    },
+    {
+      title: "Snowy",
+      imageUrl: "assets/mountain.jpeg",
+      username: "snow",
+      content: "I saw a mountain"
+    },
+    {
+      title: "Biking",
+      imageUrl: "assets/biking.jpeg",
+      username: "nature",
+      content: "I saw a bike"
+    },
+    {
+      title: "Biking",
+      imageUrl: "assets/biking.jpeg",
+      username: "nature",
+      content: "I saw a bike"
+    }
+  ];
 }
